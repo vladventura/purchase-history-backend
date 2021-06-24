ad7416fde19f3e78716e350faae93d50118c39c1
Vladimir Ventura <vladventura@outlook.es>
2021-06-23 21:07:03 -0400

    Adding logging for the user resolver

--- graphql/resolvers/userResolver.js
+++ graphql/resolvers/userResolver.js
@@ -12,6 +12,8 @@ const CryptoJS = require("crypto-js");
 const { ApolloError } = require("apollo-server-express");
 const { loggerFactory } = require("../../utils/logger");
 
+const logger = loggerFactory("info", "userResolver");
+
 const generateToken = (user) =>
   jwt.sign(
     {
@@ -24,6 +26,7 @@ const generateToken = (user) =>
   );
 
 const register = async (_, { username, email, password, confirmPassword }) => {
+  logger.log("info", `Registering user ${username} - ${email}`);
   // Validate the incoming input
   const { errors, valid } = validateRegisterInput(
     username,
@@ -40,11 +43,13 @@ const register = async (_, { username, email, password, confirmPassword }) => {
   // Find a user with this username already
   const findUsername = await UserModel.findOne({ username });
   if (findUsername) {
+    logger.log("warn", `Found ${username}`);
     errs.username = "This username is already taken";
   }
   // Find a user with this email
   const findEmail = await UserModel.findOne({ email });
   if (findEmail) {
+    logger.log("warn", `Found ${email}`);
     errs.email = "This email has already been used";
   }
   if (Object.keys(errs).length >= 1) {
@@ -52,6 +57,7 @@ const register = async (_, { username, email, password, confirmPassword }) => {
       errors: { ...errs },
     });
   }
+  logger.log("info", `Hashing password for user ${username}`);
   password = await bcrypt.hash(password, 12);
   const createdAt = new Date().toISOString();
   const newUser = new UserModel({
@@ -61,7 +67,11 @@ const register = async (_, { username, email, password, confirmPassword }) => {
     createdAt,
     validated: false,
   });
-  const user = await newUser.save();
+  logger.log("info", "Saving user on DB");
+
+  const user = await newUser
+    .save()
+    .catch((err) => logger.log("error", `While saving ${username}, ` + err));
 
   // Create the user's profile
   const profile = new UserProfileModel({
@@ -71,13 +81,18 @@ const register = async (_, { username, email, password, confirmPassword }) => {
     totalAddedItems: 0,
     user: user.id,
   });
-  await profile.save();
+  await profile
+    .save()
+    .catch((err) =>
+      logger.log("error", `While saving ${username} profile, ` + err)
+    );
   // Send the email here
   const encodedUserId = sendConfirmAccountMail(user);
   return encodedUserId;
 };
 
 const login = async (_, { username, password }) => {
+  logger.log("info", `Attempting to log in user ${username}`);
   // Validating input
   const { errors, valid } = validateLoginInput(username, password);
   if (!valid) {
@@ -88,6 +103,7 @@ const login = async (_, { username, password }) => {
   // Check if user exists
   const user = await UserModel.findOne({ username });
   if (!user) {
+    logger.log("warn", `Could not find ${username}`);
     throw new UserInputError("User not found", {
       errors: {
         general: "Couldn't find user with these credentials",
@@ -100,6 +116,7 @@ const login = async (_, { username, password }) => {
       user.id,
       process.env.JWT_SECRET
     ).toString();
+    logger.log("warn", `${usename} not validated`);
     throw new UserNotVerified("User has not been validated", {
       errors: encryptedUserId,
     });
@@ -107,6 +124,7 @@ const login = async (_, { username, password }) => {
   //  Matching credentials
   const match = await bcrypt.compare(password, user.password);
   if (!match) {
+    logger.log("warn", `Wrong password for ${username}`);
     throw new UserInputError("Wrong credentials", {
       errors: {
         general: "Couldn't log in with these credentials",
@@ -115,7 +133,9 @@ const login = async (_, { username, password }) => {
   }
   const token = generateToken(user);
   // Get the profile here
-  const profile = await UserProfileModel.findOne({ user: user.id });
+  const profile = await UserProfileModel.findOne({ user: user.id }).catch(
+    (err) => logger.log("error", `While searching profile ${username} ` + err)
+  );
   // Returning user
   const result = {
     ...user._doc,
@@ -129,11 +149,16 @@ const login = async (_, { username, password }) => {
 const confirmAccount = async (_, { userId }) => {
   // Find a user with this id (it's encoded)
   const decodedUserId = CryptoJS.AES.decrypt(userId, process.env.JWT_SECRET);
+  logger.log(
+    "info",
+    `Attempting to confirm account for ${userId} : ${decodedUserId}`
+  );
   try {
     const user = await UserModel.findOne({
       _id: decodedUserId.toString(CryptoJS.enc.Utf8),
     });
     if (!user) {
+      logger.log("warn", `${userId} : ${decodedUserId} not found`);
       throw new UserInputError("Wrong ID", {
         errors: {
           general: "Couldn't find a user with this ID",
@@ -142,21 +167,32 @@ const confirmAccount = async (_, { userId }) => {
     }
     // Check if already valid
     if (user.validated) {
+      logger.log(
+        "warn",
+        `${user.username} : ${userId} : ${decodedUserId} already validated`
+      );
       throw new UserInputError("Already validated", {
         errors: {
           general: "User is already validated",
         },
       });
     }
+    logger.log("info", `Attempting to validate user ${user.username}`);
     // Set it as validated
-    await user.updateOne({
-      validated: true,
-    });
+    await user
+      .updateOne({
+        validated: true,
+      })
+      .catch((err) =>
+        logger.log("error", `${user.username} could not validate` + err)
+      );
 
     // Return user
     const token = generateToken(user);
     // Get the profile here
-    const profile = await UserProfileModel.findOne({ user: user.id });
+    const profile = await UserProfileModel.findOne({ user: user.id }).catch(
+      (err) => logger.log("error", `Profile not found ${user.username} ` + err)
+    );
     const result = {
       ...user._doc,
       profile,
@@ -165,6 +201,7 @@ const confirmAccount = async (_, { userId }) => {
     };
     return result;
   } catch {
+    logger.log("warn", `${userId} : ${decodedUserId} invalid ID`);
     throw new UserInputError("Invalid ID", {
       errors: {
         general: "This ID is invalid",
@@ -179,8 +216,10 @@ const resendConfirmationEmail = async (_, { userId }) => {
     userId,
     process.env.JWT_SECRET
   ).toString(CryptoJS.enc.Utf8);
+  logger.log("info", `Attempting to resend email ${userId} : ${decodedUserId}`);
   const user = await UserModel.findOne({ _id: decodedUserId });
   if (!user) {
+    logger.log("warn", `${userId} : ${decodedUserId} no id found`);
     throw new UserInputError("Not found", {
       errors: {
         general: "User with this ID could not be found",
